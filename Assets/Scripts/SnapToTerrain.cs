602e554034294c9b96bfa474dba907f9c10858e3
Coline-F <80589667+Coline-F@users.noreply.github.com>
2021-03-13 17:06:33 +0100

    test

---
+++ Assets/Scripts/SnapToTerrain.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class SnapToTerrain : MonoBehaviour
+{
+    public float offsetY = 1;
+    public bool snap = true;
+    public LayerMask groundMask;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (snap)
+        {
+            RaycastHit hit;
+            if(Physics.Raycast(transform.position + Vector3.up * 100 , Vector3.down , out hit , 1000 , groundMask))
+            {
+                transform.position = hit.point + Vector3.up * offsetY;
+            }
+        }
+    }
+}
