8bde8fd4cd9da0ea8be067eb592e13b57c1bb971
D1nma <74898947+D1nma@users.noreply.github.com>
2021-03-15 15:36:53 +0100

    Lampe à huile prefab + modif

--- Assets/Scripts/LampeHuile.cs
+++ Assets/Scripts/LampeHuile.cs
@@ -180,6 +180,7 @@ public class LampeHuile : MonoBehaviour
             {
                 currentHuile -= consume;
                 huileBar.value = currentHuile;
+                lightUp();
 
             }
             else { StopHuile(); lightDown(); }
