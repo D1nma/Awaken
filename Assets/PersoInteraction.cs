37e584ac9876e214cd3da101d58a6143bf38b1c0
D1nma <74898947+D1nma@users.noreply.github.com>
2021-03-16 17:53:51 +0100

    Avancement intéraction canne

---
+++ Assets/PersoInteraction.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PersoInteraction : MonoBehaviour
+{
+    public GameObject InteragirText;
+    private bool fait=false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (player.tag == "Player")
+        {
+            if (InteragirText != null && !fait)
+            {
+                InteragirText.gameObject.GetComponent<Text>().text = "Appuie sur E pour intéragir ("+this.gameObject.name+")";
+                InteragirText.SetActive(true);             
+            }
+        }
+    }
+}
