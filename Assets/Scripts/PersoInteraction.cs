b60d3f36f37e9b4c85ab4a4c9ace53745862caba
D1nma <74898947+D1nma@users.noreply.github.com>
2021-03-18 14:12:01 +0100

    Avancement PersoInteraction

---
+++ Assets/Scripts/PersoInteraction.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PersoInteraction : MonoBehaviour
+{
+    public GameObject InteragirText;
+    private bool fait = false, canInteract = false;
+    private Transform target;
+    public float speed = 1.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        fait = false;
+        target.transform.position = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyUp(KeyCode.E) && canInteract == true)
+        {
+            PlayersController.canControl = false;
+            fait = true;
+            float step =  speed * Time.deltaTime;
+            transform.position = Vector3.MoveTowards(transform.position, target.position, step);
+
+        }
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            if (InteragirText != null && !fait)
+            {
+                InteragirText.gameObject.GetComponent<Text>().text = "Appuie sur E pour intéragir (" + this.gameObject.name + ")";
+                InteragirText.SetActive(true);
+                canInteract = true;
+            }
+        }
+    }
+}
