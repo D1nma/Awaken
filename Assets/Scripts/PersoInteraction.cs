163a66f43ab26c51bce5c7bb44d8c353b660fdb0
D1nma <74898947+D1nma@users.noreply.github.com>
2021-03-30 14:15:51 +0200

    PersoInteraction script attente anim + assets

--- Assets/Scripts/PersoInteraction.cs
+++ Assets/Scripts/PersoInteraction.cs
@@ -5,18 +5,32 @@ using UnityEngine.UI;
 
 public class PersoInteraction : MonoBehaviour
 {
-    public GameObject InteragirText;
-    private bool fait = false, canInteract = false,startTiming = false;
-    float time;
-    private Transform target;
+    public GameObject InteragirText, player;
+    private bool fait = false, canInteract = false, startTiming = false, bouge=false,setup=false;
+    float time=0f,oldDistance=0f;
+    private Vector3 target;
     public float speed = 1.0f;
     public Inventaire invent;
+    Vector3 playerPos;
     public GameObject Tips;
     // Start is called before the first frame update
     void Start()
     {
         fait = false;
-        target.transform.position = transform.position;
+        setup = false;
+        StartCoroutine(AfterInstance());
+        bouge = false;
+
+    }
+
+    IEnumerator AfterInstance()
+    {
+        yield return new WaitForSeconds(5);
+        player = GameObject.FindGameObjectWithTag("Player");
+        if (!player)
+        {
+            Debug.Log("Il est ou le joueur? Tag le !");
+        }
         if (!Tips)
         {
             Debug.LogWarning("Ajouter les parametres necessaires au fonctionnement dans l'inspecteur..");
@@ -26,38 +40,68 @@ public class PersoInteraction : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+
+        if (!player)
+        {
+            player = GameObject.FindGameObjectWithTag("Player");
+        }
+        
         if (!invent)
         {
             invent = GameObject.Find("Inventaire").GetComponent<Inventaire>();
         }
+        if (bouge)
+        {
+            if (player && !setup)
+            {
+                playerPos = player.transform.position;
+                target = new Vector3(transform.position.x, playerPos.y, transform.position.z);
+                Debug.Log(target);
+                setup = true;
+            }
+            float step = speed * Time.deltaTime;
+            player.transform.position = Vector3.MoveTowards(player.transform.position, target, step);
+            float distance = Vector3.Distance(player.transform.position, transform.position);
+            
+            if(distance == oldDistance)
+            {
+                bouge = false;
+                PlayersController.canControl = true;
+            }
+            oldDistance = distance;
+        }
         if (Input.GetKeyUp(KeyCode.E) && canInteract == true)
         {
-            if(invent.canne == true){
+            if (invent.canne == true)
+            {
+                InteragirText.SetActive(false);
                 PlayersController.canControl = false;
                 fait = true;
-                float step =  speed * Time.deltaTime;
-                transform.position = Vector3.MoveTowards(transform.position, target.position, step);
-            }else{
-                startTiming=true;
+                bouge = true;
+            }
+            else
+            {
+                startTiming = true;
                 Tips.SetActive(true);
                 InteragirText.SetActive(false);
                 Tips.gameObject.GetComponent<Text>().text = "Il faut la canne à pêche!";
-            }           
+            }
 
         }
         if (startTiming)
-            {
-                time += Time.deltaTime;
-            }
-            if (time >= 4)
-            {
-                Tips.SetActive(false);
-                time = 0;
-                startTiming = false;
-            }
-            if(fait){
-                canInteract = false;
-            }
+        {
+            time += Time.deltaTime;
+        }
+        if (time >= 4)
+        {
+            Tips.SetActive(false);
+            time = 0;
+            startTiming = false;
+        }
+        if (fait)
+        {
+            canInteract = false;
+        }
     }
     private void OnTriggerEnter(Collider other)
     {
