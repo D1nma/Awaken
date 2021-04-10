f9a4d739640f413761ea2e33eedde934b303a963
D1nma <74898947+D1nma@users.noreply.github.com>
2021-04-10 19:17:12 +0200

    Spider running

--- Assets/Scripts/PnjSpider.cs
+++ Assets/Scripts/PnjSpider.cs
@@ -8,9 +8,10 @@ public class PnjSpider : MonoBehaviour
     Animator _animator;
     private NavMeshAgent _agent;
     public GameObject player;
-    public float PnjDistanceRun=4f;
-    float oldSpeed,x,y,z;
-    float speed;
+    public float PnjDistanceRun = 4f;
+    float oldSpeed, x, z, time, timeAlea;
+    float speedNav;
+    bool timeGo,Destination;
     Vector3 randomPosition;
     // Start is called before the first frame update
     void Start()
@@ -20,7 +21,8 @@ public class PnjSpider : MonoBehaviour
         StartCoroutine(AfterInstance());
         oldSpeed = _agent.speed;
         _agent.isStopped = true;
-        speed = _animator.GetFloat("Speed");
+        speedNav = _animator.GetFloat("Speed");
+        timeGo = false;
     }
     IEnumerator AfterInstance()
     {
@@ -35,12 +37,15 @@ public class PnjSpider : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        var ray = new Ray(this.transform.position, this.transform.forward);
-        Debug.DrawRay(this.transform.position, this.transform.forward, Color.white);
+        var ray = new Ray(this.transform.position, this.transform.forward/2);
+        Debug.DrawRay(this.transform.position, this.transform.forward/2, Color.white);
         RaycastHit hit;
         if (Physics.Raycast(ray, out hit, 5f))
         {
-            _agent.isStopped = true;
+            Debug.Log("Je bloque");
+            Vector3 dirToPlayer = transform.position - hit.transform.position;
+            Vector3 newPos = transform.position + dirToPlayer;
+            _agent.SetDestination(newPos);
         }
         //Debug.Log(_agent.destination);
         //Debug.Log(_agent.isStopped);
@@ -50,37 +55,63 @@ public class PnjSpider : MonoBehaviour
 
             if (distance < PnjDistanceRun)
             {
+                _animator.SetBool("Walk", true);
                 Vector3 dirToPlayer = transform.position - player.transform.position;
                 Vector3 newPos = transform.position + dirToPlayer;
 
                 _agent.SetDestination(newPos);
                 _agent.isStopped = false;
             }
-            if(distance < PnjDistanceRun / 4)
+            if (distance < PnjDistanceRun / 4)
             {
                 _agent.speed = _agent.speed * 2;
+                _animator.SetFloat("Speed",speedNav *2);
             }
             else
             {
                 _agent.speed = oldSpeed;
+                _animator.SetFloat("Speed", speedNav /2);
             }
+        }
+        if (timeGo)
+        {
+            time += Time.deltaTime;
+        }
+        if(time >= timeAlea)
+        {
+            _animator.SetBool("Walk", false);
+            timeGo = false;
+            time = 0;
+            _agent.isStopped = true;
+            Destination = true;
         }
         if (_agent.isStopped)
         {
-            x = Random.Range(-25, 26);
-            z = Random.Range(-25, 26);
+            _animator.SetBool("Walk", false);
+            timeAlea = Random.Range(2f, 6f);
+            timeGo = true;
+        }
+        if (Destination && _agent.isStopped)
+        {
+            Debug.Log("je trouve une destination");
+            x = Random.Range(-10, 10);
+            z = Random.Range(-10, 10);
             randomPosition = new Vector3(x, transform.position.y, z);
             _agent.SetDestination(randomPosition);
             _agent.isStopped = false;
-        }
-        if(_agent.remainingDistance > 0.1f)
-        {
             _animator.SetBool("Walk", true);
+            Destination = false;
+        }
+        
+        if (_agent.remainingDistance > 0.1f)
+        {
+            //_animator.SetBool("Walk", true);
         }
         else
         {
+            Debug.Log("Je suis proche");
             _animator.SetBool("Walk", false);
-            _agent.isStopped = true;
+            _agent.isStopped = true;   
         }
     }
 }
