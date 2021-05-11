3e4035d636f3b3d0a7f387aac984ace82c42c13c
D1nma <74898947+D1nma@users.noreply.github.com>
2021-05-11 12:36:08 +0200

    detection lumière et stress fix + mort

--- Assets/Scripts/PlayerStress.cs
+++ Assets/Scripts/PlayerStress.cs
@@ -7,16 +7,18 @@ public class PlayerStress : MonoBehaviour
     public float maxStress = 100;
     public float currentStress;
     GameObject player;
+    GameManager gm;
 
     public StressBar stressBar;
     public bool dead = false;
     public bool NotSafe = true;
     public float lookRadius = 8f;
     public float DiminutionStress = 0.1f;
-    public float minRandom = 15f, maxRandom = 25f,LaValeur,speed=0.02f;
+    public float minRandom = 15f, maxRandom = 25f,LaValeur,speed=0.5f;
 
     void Start()
     {
+        gm = GameObject.FindGameObjectWithTag("GM").GetComponent<GameManager>();
         GameObject bar = GameObject.Find("StressBar");
         stressBar = bar.GetComponent<StressBar>();
         currentStress = 0;
@@ -25,6 +27,7 @@ public class PlayerStress : MonoBehaviour
         StartCoroutine(AfterInstance());
         NotSafe = true;
         NewValue();
+        dead = false;
     }
 
     IEnumerator AfterInstance()
@@ -40,7 +43,7 @@ public class PlayerStress : MonoBehaviour
     {
         if (stressBar)
         {
-            if (!player)
+            if (!player || dead)
             {
                 stressBar.gameObject.SetActive(false);
             }
@@ -48,6 +51,39 @@ public class PlayerStress : MonoBehaviour
             {
                 stressBar.gameObject.SetActive(true);
                 stressBar.SetStress(currentStress);
+                if (Input.GetKeyDown(KeyCode.Space))
+                {
+                    StressUp(20);
+                }
+                if (currentStress > 0 && currentStress != maxStress && !NotSafe)
+                {
+                    StressDown();
+                }
+                if (currentStress < 0)
+                {
+                    currentStress = 0;
+                }
+                if (currentStress >= maxStress)
+                {
+                    stressBar.gameObject.SetActive(false);
+                    currentStress = 0;
+                    //stressBar.SetStress(currentStress); //annule la réactualisation 
+                    GameManager.gameOver = true;
+                    dead = true;
+
+                }
+                if (NotSafe)
+                {
+                    if (currentStress >= LaValeur)
+                    {
+                        currentStress -= LaValeur * speed * Time.deltaTime;
+                    }
+                    if (currentStress <= LaValeur)
+                    {
+                        currentStress += LaValeur * speed * Time.deltaTime;
+                    }
+
+                }
             }
         }
         else
@@ -55,44 +91,18 @@ public class PlayerStress : MonoBehaviour
             GameObject bar = GameObject.Find("StressBar");
             stressBar = bar.GetComponent<StressBar>();
         }
-        if (Input.GetKeyDown(KeyCode.Space))
+        if(GameManager.gameOver == false)
         {
-            StressUp(20);
+            dead = false;
         }
+        
 
         /*if (currentStress <= maxStress && !dead)
         {
             StressUp(20);
             timeStress = 0;
         }*/
-        if (currentStress > 0 && currentStress != maxStress && !NotSafe)
-        {
-            StressDown();
-        }
-        if (currentStress < 0)
-        {
-            currentStress = 0;
-        }
-        if (currentStress >= maxStress)
-        {
-            currentStress = 0;
-            //stressBar.SetStress(currentStress); //annule la réactualisation 
-            GameManager.gameOver = true;
-            dead = true;
-
-        }
-        if (NotSafe)
-        {
-            if (currentStress >= LaValeur)
-            {
-                currentStress -= LaValeur *speed * Time.deltaTime;
-            }
-            else
-            {
-                currentStress += LaValeur *speed * Time.deltaTime;
-            }
-            
-        }
+        
     }
 
     void NewValue()
